0d2bf9cbda0b93616909573104bc3fe53ccc40bd
Darusia <dariapionk13@gmail.com>
2021-11-30 13:32:22 +0100

    Zadanie 2

--- projekt/tests/FizzBuzzParameterizedPackage.py
+++ projekt/tests/FizzBuzzParameterizedPackage.py
@@ -2,6 +2,36 @@ import unittest
 from sample.FizzBuzz import *
 from parameterized import parameterized, parameterized_class
 
+class FizzBuzzParameterizedPackage(unittest.TestCase):
+
+    def setUp(self):
+        self.tmp = FizzBuzz()
+
+    @parameterized.expand ([
+    (5, "Buzz"),
+    (3, "Fizz"),
+    (15, "FizzBuzz"),
+    (-5, "Buzz"),
+    (-3, "Fizz"),
+    (-30, "FizzBuzz"),
+    (0, "FizzBuzz"),
+    (7, "7"),
+    (-7, "-7"),
+    ])
+    def test_parameterized_correct(self, number, expected):
+        self.assertEqual(self.tmp.game(number), expected)
+
+    @parameterized.expand([
+    ('game', []),
+    ('game', {}),
+    ('game', 3.14)
+])
+    def test_parameterized_class_wrong(self, game, argument):
+        self.assertRaises(ValueError, self.tmp.game, argument)
+
+
+
+#Dodatkowe zadanie
 @parameterized_class(('number', 'expected'), [
     (5, "Buzz"),
     (3, "Fizz"),
@@ -13,11 +43,12 @@ from parameterized import parameterized, parameterized_class
     (7, "7"),
     (-7, "-7"),
 ])
-class FizzBuzzParameterizedPackage(unittest.TestCase):
+
+class FizzBuzzParameterizedPackage_2(unittest.TestCase):
     def setUp(self):
         self.tmp = FizzBuzz()
 
-    def test_parameterized_correct(self):
+    def test_parameterized_class_correct(self):
         self.assertEqual(self.tmp.game(self.number), self.expected)
 
 
@@ -26,13 +57,16 @@ class FizzBuzzParameterizedPackage(unittest.TestCase):
     ('game',{}),
     ('game',3.14)
 ])
-class FizzBuzzParameterizedPackage_2(unittest.TestCase):
+class FizzBuzzParameterizedPackage_3(unittest.TestCase):
 
     def setUp(self):
         self.tmp = FizzBuzz()
 
-    def test_parameterized_wrong(self):
+    def test_parameterized_class_wrong(self):
         self.assertRaises(ValueError, self.tmp.game, self.argument )
+
+
+
 
 
 if __name__ == '__main__':
